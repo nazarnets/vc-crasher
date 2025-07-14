15dd3f6835230c8b5604a848821558f01756b190
nazareth <rot@x0q.net>
2025-07-14 23:35:47 +0000

    Create main.py

---
+++ main.py
@@ -0,0 +1,19 @@
+import httpx
+import time
+
+nazareth = httpx.Client()
+chanid = input(">")
+token = ""
+auth = {"Authorization": token}
+list = ["brazil", "hongkong", "india", "japan", "rotterdam", "singapore"]
+while True:
+    for reg in list:
+        sex = nazareth.patch(f"https://discord.com/api/v9/channels/{chanid}/call", headers=auth, json={"region": reg})
+        time.sleep(3)
+        if sex.status_code == 429:
+            jizz = sex.json()
+            sperm = jizz.get("retry_after")
+            print(f"{reg} : {sperm}")
+            time.sleep(retry)
+        else:
+            print(f"{reg} : {sex.status_code}")
